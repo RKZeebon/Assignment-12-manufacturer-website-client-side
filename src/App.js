0f637da8299b6195a5cc3d16c5c2aecb9c906bab
RKZeebon <rkzeebon@gmail.com>
2022-05-22 00:12:45 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/HomePage/Home';
 import Navbar from './Components/SharedPages/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/HomePage/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bannerimg from '../../../Assets/images/banner-img.jpg'
+
+const Banner = () => {
+    return (
+        <div className="min-h-full p-5 w-5/6 mx-auto bg-white rounded-lg">
+            <div className="hero-content flex-col lg:flex-row-reverse max-w-full my-12">
+                <div>
+                    <img src={bannerimg} alt='' />
+                </div>
+                <div className=''>
+                    <h1 className="text-5xl font-bold">Get Your Masonry tools!</h1>
+                    <p className="py-6">We are here to provide your required tools for your business. We manufacture all kind of masonry tools by maintaining the quality.</p>
+                    <button className="btn btn-primary px-8 text-lg uppercase font-semibold bg-gradient-to-r from-primary to-secondary">Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
