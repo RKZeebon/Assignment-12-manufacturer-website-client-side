2f092ac42bac605c5633c75527028d6e19237384
RKZeebon <rkzeebon@gmail.com>
2022-05-24 11:08:46 +0600

    users handle done

--- src/Components/Dashboard/AllUsers/AllUsers.js
+++ src/Components/Dashboard/AllUsers/AllUsers.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+import auth from '../../../firebase.init';
+import Loading from '../../SharedPages/Loading';
+import Alluser from './Alluser';
 
 const AllUsers = () => {
+    const [user] = useAuthState(auth);
+
+    const { isLoading, data: users, refetch } = useQuery('allUsers', () => fetch('http://localhost:5000/allUser', {
+        headers: {
+            'authorization': localStorage.getItem('token'),
+            'email': user.email
+        }
+    })
+        .then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading />
+    }
+    refetch()
     return (
         <div>
-            <h1>This Is all User</h1>
+            <h1 className='text-xl text-center font-semibold mt-5 mb-2'>Users</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+
+                    <thead>
+                        <tr>
+                            <th>Sl No.</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, index) => <Alluser
+                                key={user._id}
+                                user={user}
+                                index={index}
+                            ></Alluser>)
+                        }
+                    </tbody>
+                </table>
+
+            </div>
+            <ToastContainer />
         </div>
     );
 };
---
+++ src/Components/Dashboard/AllUsers/Alluser.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const Alluser = ({ user, index }) => {
+    const { name, email, role } = user;
+    const newRole = 'admin'
+    const handleMakeAdmin = email => {
+        fetch(`http://localhost:5000/user?email=${email}`, {
+            method: "PUT",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ role: newRole })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success("Action done Successfully")
+                }
+            })
+    }
+
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>
+                {
+                    role ? <span className='bg-secondary px-7 py-2 rounded-lg text-green-700'>Already admin</span> : <button onClick={() => handleMakeAdmin(email)} className='btn btn-primary'>Make Admin</button>
+                }
+            </td>
+
+        </tr>
+    );
+};
+
+export default Alluser;
